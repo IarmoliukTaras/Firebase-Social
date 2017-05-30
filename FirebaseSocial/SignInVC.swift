7ea563b594d3e110976e2bd7f7cecc96ae8a1389
Iarmoliuk Taras <iarmoliuktaras@gmail.com>
2017-05-30 20:07:37 +0300

    Completed authenticate with Facebook

--- FirebaseSocial/SignInVC.swift
+++ FirebaseSocial/SignInVC.swift
@@ -7,9 +7,39 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class SignInVC: UIViewController {
 
+    @IBAction func facebookButtonPressed(_ sender: AnyObject) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) {(result, error) in
+            if error != nil {
+                print("JESS: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("JESS: User cancelled Facebook authenticate")
+            } else {
+                print("JESS: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuthenticate(credential)
+            }
+        }
+    }
+    
+    func firebaseAuthenticate(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("JESS: Unable to authenticate with Facebook - \(error)")
+            } else {
+                print("JESS: Successfully authenticated with Firebase")
+                
+            }
+            
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
--- Podfile
+++ Podfile
@@ -10,5 +10,6 @@ target 'FirebaseSocial' do
   pod ‘Firebase/Auth’
   pod ‘Firebase/Database’
   pod ‘Firebase/Storage’
+  pod ‘FBSDKLoginKit’
 
 end
